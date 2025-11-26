5ac6a2c3f65002035ffce610ba8e724e219c9bf5
Ren0g <98395283+Ren0g@users.noreply.github.com>
2025-11-26 17:12:34 +0100

    Delete app/pioniri/page.tsx

--- app/pioniri/page.tsx
+++
@@ -1,9 +0,0 @@
-import LeagueView from "@/components/LeagueView";
-
-export default function PioniriPage() {
-  return (
-    <div className="p-4">
-      <LeagueView leagueCode="PIONIRI" />
-    </div>
-  );
-}
