dbc232b3b60ee4edfdda4a415af9f8b20f602c9a
Ren0g <98395283+Ren0g@users.noreply.github.com>
2025-11-26 17:12:15 +0100

    Delete app/mladji/page.tsx

--- app/mladji/page.tsx
+++
@@ -1,9 +0,0 @@
-import LeagueView from "@/components/LeagueView";
-
-export default function MladjiPioniriPage() {
-  return (
-    <div className="p-4">
-      <LeagueView leagueCode="MLADJI" />
-    </div>
-  );
-}
