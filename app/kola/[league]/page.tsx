f9b80ebd724a01f9158307d885a28e30825da503
Ren0g <98395283+Ren0g@users.noreply.github.com>
2025-11-27 23:05:33 +0100

    Delete app/kola/[league]/page.tsx

--- app/kola/[league]/page.tsx
+++
@@ -1,6 +0,0 @@
-import AllRoundsClient from "./client";
-
-export default function Page({ params }: { params: { league: string } }) {
-  const code = (params.league || "").toUpperCase();
-  return <AllRoundsClient leagueCode={code} />;
-}
