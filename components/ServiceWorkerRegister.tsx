6accf142254428b07535da1cee08d9609864d544
Ren0g <98395283+Ren0g@users.noreply.github.com>
2025-11-29 01:31:07 +0100

    Create ServiceWorkerRegister.tsx

---
+++ components/ServiceWorkerRegister.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function ServiceWorkerRegister() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .catch((err) => {
+          console.error("SW register error:", err);
+        });
+    }
+  }, []);
+
+  return null;
+}
